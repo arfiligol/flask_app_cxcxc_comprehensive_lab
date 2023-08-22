e01b81d00560186665c3d5ecc4f01909efcf7d05
arfiligol <arfiligol@gmail.com>
2023-08-22 20:05:41 +0800

    cloud_run_with_firestore.py 修正

--- cloud_run_with_firestore.py
+++ cloud_run_with_firestore.py
@@ -30,7 +30,7 @@ def index():
 
     # 寫入 users.json 到 db
     for item in data:
-        db.collection("users").document(item["id"]).set(item)
+        db.collection("users").document(str(item["id"])).set(item)
 
     # 回傳寫入的資料
     return jsonify(data)
