901c21b5f15350323aaf0b23a506bf937484958f
arfiligol <arfiligol@gmail.com>
2023-08-24 19:49:53 +0800

    alternate cloud_run_with_firestore.py

--- cloud_run_with_firestore.py
+++ cloud_run_with_firestore.py
@@ -1,5 +1,3 @@
-import json
-import os
 from google.cloud import firestore
 from dotenv import load_dotenv
 load_dotenv()
@@ -17,19 +15,24 @@ db = firestore.Client() # project 要與 firebase 運行時設定的一致，才
 @app.route('/')
 def index():
     # 模擬一些計算工作
+    print("Emulating some calculation work...")
     total = 0
     for i in range(10000):
         total += i
 
     # 模擬一些 I/O 等待
+    print("Emualting some waiting job...")
     time.sleep(0.01)
 
     # 讀取 users.json 的內容
+    print("reading user.json")
     with open("./users.json") as jsonFile:
         data = json.load(jsonFile)
 
     # 寫入 users.json 到 db
+    print("Importing/Updating data to firestore db...")
     for item in data:
+        print("Insert/Update {}!".format(str(item["id"])))
         db.collection("users").document(str(item["id"])).set(item)
 
     # 回傳寫入的資料
